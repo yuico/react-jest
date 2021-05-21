56094d2fe52f917d4b581f7c603c855691ab2116
nokdoo00 <nokdoo00@gmail.com>
2021-05-21 18:45:54 -0400

    toBeFalsy(), toBeTruthy(), toHaveLenght(), toContain(), not, toMatch()

--- test.js
+++ test.js
@@ -22,3 +22,21 @@ test("return a user object", () => {
     email: `user1@test.com`
   })
 });
+
+test("number 0 is falsy but string '0' is truthy", () => {
+  expect(0).toBeFalsy();
+  expect("0").toBeTruthy();
+})
+
+test("array", () => {
+  const colors = ["Red", "Green", "Pink"];
+  expect(colors).toHaveLength(3);
+  expect(colors).toContain("Green");
+  expect(colors).not.toContain("Blue");
+})
+
+//regex with toMatch()
+test("string", () => {
+  expect(getUser(1).email).toBe("user1@test.com")
+  expect(getUser(2).email).toMatch(/.*test.com$/)
+})
